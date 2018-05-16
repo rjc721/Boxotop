9039aaca45afa265e98fce7c17aefe3cef5e1925
Ryan Chingway <rchingway@gmail.com>
2018-05-16 13:24:06 -0500

    Superstitious commit

--- Boxotop/Controller/BoxOfficeTableViewController.swift
+++ Boxotop/Controller/BoxOfficeTableViewController.swift
@@ -28,7 +28,7 @@ class BoxOfficeTableViewController: UITableViewController {
     let MOVIE_GLU_CLIENT = "WHYO"
     let MOVIE_GLU_AUTH = "Basic V0hZTzpJSkxXbk53a0l5cXg="
     let MOVIE_GLU_VERSION = "v102"
-   
+    
     let navBarGreen = UIColor(hexString: "5C9E41")      //Green colors from the test design documentation
     let tableCellGreen = UIColor(hexString: "BFDBB3")   //given from "EliteDesign"
     
@@ -36,7 +36,7 @@ class BoxOfficeTableViewController: UITableViewController {
     @IBOutlet weak var refreshButton: UIBarButtonItem!
     
     var movies = ["Deadpool 2", "Avengers: Infinity War", "Sherlock Gnomes", "I Feel Pretty", "Life of the Party", "Breaking In", "The Guernsey Literary and Potato Peel Pie Society", "A Quiet Place", "Rampage", "Entebbe", "Peter Rabbit", "The Strangers: Prey at Night", "The Greatest Showman", "Tully", "Truth or Dare"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80.0
@@ -48,42 +48,42 @@ class BoxOfficeTableViewController: UITableViewController {
         
         if realm.isEmpty {
             SVProgressHUD.show()    //Show loading to user
-            loadBoxOfficeFilms()
+            loadBoxOfficeFilms()    //Database empty on first use, load Films
         } else {
             filmDatabase = realm.objects(Film.self)
             boxOfficeFilms = filmDatabase?.filter("isNowPlaying == %@", true).sorted(byKeyPath: "title")
             tableViewCache = boxOfficeFilms
         }
-       
+        
     }
-
+    
     @IBAction func refreshTapped(_ sender: UIBarButtonItem) {
         
         SVProgressHUD.show()
         searchBar.text = ""
         searchBar.resignFirstResponder()
         
-        loadBoxOfficeFilms()
+        loadBoxOfficeFilms()    //Refresh button reloads movies Now Playing
     }
     
-
+    
     // MARK: - Table view methods
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if let filmCount = boxOfficeFilms?.count {
-
+            
             return filmCount > 0 ? filmCount : 1    //List exists, show list OR Search cell
         }
-
+        
         return 1        //List not loaded -> Show Loading cell
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "filmCell", for: indexPath)
-      
+        
         if boxOfficeFilms != nil {
             if boxOfficeFilms!.count == 0 {     //Case - filtered but did not find
                 cell.imageView?.image = nil
@@ -119,29 +119,125 @@ class BoxOfficeTableViewController: UITableViewController {
     //MARK: Networking with Alamofire
     
     func loadBoxOfficeFilms() {
+        searchOMDB(titles: movies, boxOfficeSearch: true)
         
-        loadFromMDB(titles: movies)
+        //loadFromMDB(titles: movies)
         
-//        let movieGluHeaders = ["client" : MOVIE_GLU_CLIENT, "x-api-key" : MOVIE_GLU_KEY, "Authorization" : MOVIE_GLU_AUTH, "api-version" : MOVIE_GLU_VERSION]
-//        let nowPlayingURL = MOVIE_GLU_URL + "filmsNowShowing/?n=15"
-//
-//        Alamofire.request(nowPlayingURL, method: .get, headers: movieGluHeaders).responseJSON { response in
-//
-//            if response.result.isSuccess {
-//
-//                let nowPlayingJSON : JSON = JSON(response.result.value!)
-//                self.decodeMovieGluJSON(nowPlayingJSON)
-//
-//            }
-//        }
+        //        let movieGluHeaders = ["client" : MOVIE_GLU_CLIENT, "x-api-key" : MOVIE_GLU_KEY, "Authorization" : MOVIE_GLU_AUTH, "api-version" : MOVIE_GLU_VERSION]
+        //        let nowPlayingURL = MOVIE_GLU_URL + "filmsNowShowing/?n=15"
+        //
+        //        Alamofire.request(nowPlayingURL, method: .get, headers: movieGluHeaders).responseJSON { response in
+        //
+        //            if response.result.isSuccess {
+        //
+        //                let nowPlayingJSON : JSON = JSON(response.result.value!)
+        //                self.decodeMovieGluJSON(nowPlayingJSON)
+        //
+        //            }
+        //        }
     }
     
-    func loadFromMDB(titles: [String]) {
-        print("Titles passed: \(titles)")
-    
+    func searchOMDB(titles: [String], boxOfficeSearch: Bool) {
+        
         for title in titles {
             let adjustedTitle = title.replacingOccurrences(of: " ", with: "-")
-            let filmParams = ["t" : adjustedTitle, "apikey" : OMDB_API_KEY]
+            let filmParams = ["s" : adjustedTitle, "type" : "movie", "apikey" : OMDB_API_KEY]
+            
+            Alamofire.request(OMDB_URL, method: .get, parameters: filmParams).responseJSON { response in
+                
+                if response.result.isSuccess {
+                    
+                    let searchResultsJSON : JSON = JSON(response.result.value!)
+                    
+                    if let success = searchResultsJSON["Response"].string {
+                        if success == "True" {
+                            self.parseSearchResultsJSON(searchResultsJSON, searchQuery: title, boxOfficeSearch: boxOfficeSearch)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func parseSearchResultsJSON(_ json: JSON, searchQuery: String, boxOfficeSearch: Bool) {
+        if let count = Int(json["totalResults"].string!) {
+            
+            var searchResults = [SearchResultOMDB]()    //Using these results to find true box office titles
+            var searchIDResults = [String]()            //Using these IDS for a generic user search
+            
+            let resultsCount = count <= 10 ? count : 10     //Search JSON contains max of 10 results
+            
+            for index in 0..<resultsCount {
+                
+                let searchResult = SearchResultOMDB()
+                searchResult.title = json["Search"][index]["Title"].string!
+                searchResult.imdbID = json["Search"][index]["imdbID"].string!
+                searchResult.yearReleased = json["Search"][index]["Year"].string!
+                searchResult.relevanceScore = 0
+                searchResult.searchQuery = searchQuery
+                
+                searchIDResults.append(searchResult.imdbID)
+                searchResults.append(searchResult)
+            }
+            
+            if !boxOfficeSearch {
+                loadFromOMDB(imdbIDs: searchIDResults)
+            } else {
+                checkSearchRelevance(results: searchResults)
+            }
+            
+        }
+    }
+        
+    func checkSearchRelevance(results: [SearchResultOMDB]) {
+        
+        if results.count == 1 {
+            
+            if let onlyResult = results.first?.imdbID {
+                loadFromOMDB(imdbIDs: [onlyResult])
+            }
+            
+        }
+        else if results.count > 1 {
+           
+            let date = Date()
+            let calendar = Calendar.current
+            let year = calendar.component(.year, from: date)
+            
+            for result in results {
+                if let releaseYearInt = Int(result.yearReleased) {
+                    print("Release year Int: \(releaseYearInt)")
+                    switch releaseYearInt {
+                        
+                    case year:  result.relevanceScore += 3
+                    case year - 1:  result.relevanceScore += 2
+                    case year - 2:  result.relevanceScore += 1
+                    default:    break
+                    
+                    }
+            }
+                
+                if result.title == result.searchQuery {
+                    result.relevanceScore += 2
+                }
+            }
+            
+            let sortedResults = results.sorted(by: {$0.relevanceScore > $1.relevanceScore})
+            
+            if let bestResult = sortedResults.first?.imdbID {
+                loadFromOMDB(imdbIDs: [bestResult])
+            }
+            
+        }
+    }
+    
+    func loadFromOMDB(imdbIDs: [String]) {
+        
+        print("IDs passed: \(imdbIDs)")
+        
+        for imdbID in imdbIDs {
+            
+            let filmParams = ["i" : imdbID, "apikey" : OMDB_API_KEY]
             
             Alamofire.request(OMDB_URL, method: .get, parameters: filmParams).responseJSON { response in
                 
@@ -149,8 +245,15 @@ class BoxOfficeTableViewController: UITableViewController {
                     
                     let filmJSON : JSON = JSON(response.result.value!)
                     
-                    self.decodeMDBJSON(filmJSON)
-                    print("JSON passed: \(filmJSON)")
+                    if let success = filmJSON["Response"].string {
+                        
+                        if success == "True" {
+                            
+                            self.decodeOMDBJSON(filmJSON)
+                            print("JSON passed: \(filmJSON)")
+                            
+                        }
+                    }
                 }
             }
         }
@@ -168,22 +271,16 @@ class BoxOfficeTableViewController: UITableViewController {
             nowPlayingTitles.append(title)
         }
         
-        loadFromMDB(titles: nowPlayingTitles)
+        searchOMDB(titles: nowPlayingTitles, boxOfficeSearch: true)
         
     }
     
     
     // Movie Database JSON -- Create Film objects, load into Tableview
-    func decodeMDBJSON(_ json : JSON, isNowPlaying: Bool = true) {
+    func decodeOMDBJSON(_ json : JSON, isNowPlaying: Bool = true) {
         
         let newFilm = Film()
         
-//        if let title = json["Title"].string {
-//            newFilm.title = title
-//        } else {
-//            print(json["Title"].string)
-//            print("****JSON: \(json)")
-//        }
         newFilm.title = json["Title"].string!
         newFilm.director = json["Director"].string!
         newFilm.cast = json["Actors"].string!               //Using explicit unwrapping because JSON is
@@ -203,7 +300,7 @@ class BoxOfficeTableViewController: UITableViewController {
         }
         
         if let posterImageURL = json["Poster"].string {
-    
+            
             let url = URL(string: posterImageURL)
             
             do {
@@ -230,7 +327,7 @@ class BoxOfficeTableViewController: UITableViewController {
                 return
             }
         }
-      
+        
         //If it doesn't exist, add it to database
         do {
             try realm.write {
@@ -238,9 +335,9 @@ class BoxOfficeTableViewController: UITableViewController {
                 filmDatabase = realm.objects(Film.self)
             }
         } catch {
-            print("Error loading film into Realm")
+            fatalError("Error loading film into Realm")
         }
-
+        
         boxOfficeFilms = realm.objects(Film.self).sorted(byKeyPath: "title")
         tableViewCache = boxOfficeFilms
         
@@ -249,8 +346,7 @@ class BoxOfficeTableViewController: UITableViewController {
     }
     
     // MARK: - Navigation
-
-   
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         guard let destinationVC = segue.destination as? FilmViewController else {fatalError("Wrong destination view controller")}
@@ -265,18 +361,18 @@ class BoxOfficeTableViewController: UITableViewController {
     
 }
 
-    //MARK: Search Bar Delegate Methods
+//MARK: Search Bar Delegate Methods
 
 extension BoxOfficeTableViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    
+        
         DispatchQueue.main.async {
             searchBar.resignFirstResponder()
         }
         
     }
-
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
         let filterPredicate = NSPredicate(format: "title CONTAINS[cd] %@ || director CONTAINS[cd] %@ || %K CONTAINS[cd] %@", argumentArray: [searchBar.text!, searchBar.text!, "cast", searchBar.text!])
@@ -291,5 +387,5 @@ extension BoxOfficeTableViewController: UISearchBarDelegate {
         }
         
     }
-
+    
 }
---
+++ Boxotop/Model/SearchResultOMDB.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultOMDB.swift
+//  Boxotop
+//
+//  Created by Ryan Chingway on 5/16/18.
+//  Copyright © 2018 Ryan Chingway. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultOMDB {
+    var searchQuery: String = ""
+    var title: String = ""
+    var imdbID: String = ""
+    var yearReleased: String = ""
+    var relevanceScore: Int = 0
+}
